436e5b998ede65b0ff777249cbabdf2407ed0eda
Jamie Jansen <32266479+jajan20@users.noreply.github.com>
2018-04-15 14:46:03 +0200

    Added sticky headers
    - Added sticky headers with javascript.

--- opdracht3/public/js/script.js
+++ opdracht3/public/js/script.js
@@ -1,49 +1,50 @@
-// windows.onload = function() {
-// 	if (javascript geladen wordt) {
-// 		laat dan de zoek balk zien
-// 	} else {
-// 		anders display none je de zoekbalk
-// 	}
-// }
+window.addEventListener('scroll', function (e){
+	let top = this.scrollY
+	let stickyHeaders = document.querySelectorAll('.letter-container')
+	let delHeader = false
+
+	for (i = stickyHeaders.length-1; i > -1 ; i--) {
+		let header = stickyHeaders[i]
+		let letter = header.querySelector('.follow')
+		
+		if (i == 0) {
+
+		}
+		if (!delHeader){ 
+			letter.classList.remove('delete')
+			if (header.getBoundingClientRect().top <= 0) {
+				letter.classList.add('fixed')
+				delHeader = true
+			} else {
+				letter.classList.remove('fixed')
+			}
+		} else {
+			letter.classList.add('delete')
+		}
+	}
+})
 
 var search = document.getElementById('search-bar')
 
 search.onkeyup = function(event) {
 	var filter = search.value.toUpperCase()
-	var ol = document.getElementById('list')
-	var li = ol.getElementsByTagName('li')
+	var contacts = document.getElementById('list')
+	var persons = contacts.getElementsByTagName('li')
 
-	for (i=0; i < li.length; i++) {
-		a = li[i].getElementsByTagName("a")[0]
+	for (i=0; i < persons.length; i++) {
+		a = persons[i].getElementsByTagName("a")[0]
 		if(a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-			
-			li[i].style.display = ""
+			persons[i].setAttribute('class', '')
 		} else {
+			persons[i].setAttribute('class', 'hidden')
+		} if (search.value === '') {
 			
-			li[i].style.display = "none"
 		}
 	}
 
 }
 
-// // When the user scrolls the page, execute myFunction 
-// window.onscroll = function() {
-// 	var header = document.getElementById("myHeader")
 
-// 	// Get the offset position of the navbar
-// 	var sticky = header.offsetTop
-
-// 	// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-
-//   	if (window.pageYOffset >= sticky) {
-//     	header.classList.add("sticky")
-//   	} else {
-//     header.classList.remove("sticky")
-//   }
-
-// }
-
-// // Get the header
 
 
 
